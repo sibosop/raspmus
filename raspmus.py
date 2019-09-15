a8dbb6a48ef527aa819b409c2bb08611f1efb7ba
brian reinbolt <brian@eastshore.com>
2019-09-15 11:33:15 -0700

    got rapmus music player working

--- raspmus.py
+++ raspmus.py
@@ -1,27 +1,29 @@
 #!/usr/bin/env python
 import os
 import sys
+import pygame
 os.environ['DISPLAY']=":0.0"
 os.chdir(os.path.dirname(sys.argv[0]))
 pname = sys.argv[0]
 home = os.environ['HOME']
 sys.path.append("sibcommon")
-defaultSpecPath = "%s/%s"%("specs","raspmus.json")
+defaultSpecPath = "%s/%s"%("speclib","raspmus.json")
 import datetime
 import time
 import argparse
 from hosts import Hosts
 import subprocess
+import traceback
 from asoundConfig import setVolume
 from utils import print_dbg
 from utils import setDebug
 from server import Server
 from specs import Specs
 import json
+from soundTrack import SoundTrackManager
+from musicPlayer import MusicPlayer
 
-numGardenThreads=1
 gardenExit = 0
-isMasterFlag = False;
 
 def doProbe(args):
   state = {}
@@ -31,49 +33,53 @@ def doProbe(args):
     state[k]=attr[k]
   return json.dumps(state)
 
+
+
 if __name__ == '__main__':
   try:
     print("%s at %s"%(pname,datetime.datetime.fromtimestamp(time.time()).strftime('%Y-%m-%d %H:%M:%S')))
+    pygame.mixer.pre_init(frequency=44100, size=-16, channels=2, buffer=4096)
+    pygame.init()
     parser = argparse.ArgumentParser() 
-    parser.add_argument('-d','--debug', action = 'store_true',help='set debug')
+    parser.add_argument('-v','--verbose', action = 'store_true',help='set debug')
     parser.add_argument('-c','--config',nargs=1,type=str,default=[defaultSpecPath],help='specify different config file')
+    parser.add_argument('-s','--soundDir',nargs=1,help='specify sound file directory')
     args = parser.parse_args()
-    setDebug(args.debug)
+    setDebug(args.verbose)
     print_dbg("config path %s"%args.config[0])
     specs = Specs(args.config[0]).s
-  except Exception, e:
-    print("config error:"+str(e))
-    exit(5)
+    soundDir = specs['defaultSoundDir']
+    if args.soundDir is not None:
+      soundDir = args.soundDir[0]
+      
+    if "masterVolume" in specs:
+      setVolume(specs['masterVolume'])
+    
+    if Hosts().getLocalAttr('hasServer'):
+      sst = Server(8080)
+      sst.registerCommand("Probe",doProbe)
+      sst.setDaemon(True)
+      sst.start()
+      if Hosts().getLocalAttr('hasMusic'):
+        SoundTrackManager(soundDir).changeNumSoundThreads(specs['numMusicThreads'])
 
-
-  if "masterVolume" in specs:
-    setVolume(specs['masterVolume'])
-  isMasterFlag =Hosts().getLocalAttr('isMaster')
-  print_dbg("isMaster: %s"%(isMasterFlag))
-  sst = Server(8080)
-  sst.registerCommand("Probe",doProbe)
-  sst.setDaemon(True)
-  sst.start()
-#  soundTrack.setup()
-#  gardenTrack.changeNumGardenThreads(numGardenThreads)
-#  speakThread = gardenSpeak.gardenSpeakThread()
-#  speakThread.setDaemon(True)
-#  speakThread.start()
-#  if isMasterFlag:
-#    print("starting player")
-#    pt = player.playerThread()
-#    pt.setDaemon(True)
-#    pt.start()
-  while gardenExit == 0:
-    try:
-      time.sleep(5)
-    except KeyboardInterrupt:
-      print("%s: keyboard interrupt"%pname)
-      gardenExit = 5
-      break
-    except Exception as e:
-      print("%s"%e)
-      break
+    if Hosts().getLocalAttr('hasMusicPlayer'):
+      print("starting Music Player")
+      pt = MusicPlayer()
+      pt.setDaemon(True)
+      pt.start()
+    
+    while gardenExit == 0:
+      try:
+        time.sleep(5)
+      except KeyboardInterrupt:
+        print("%s: keyboard interrupt"%pname)
+        gardenExit = 5
+        break
+  except Exception as e:
+    print("%s"%e)
+    traceback.print_exc()
+    gardenExit = 5
 
   print("%s exiting"%pname)
   exit(gardenExit)
